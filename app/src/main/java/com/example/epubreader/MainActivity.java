3a66c0b14ea56fd8e5046931a4c498bc15911b50
vexades <ionutnato@gmail.com>
2020-09-25 08:17:34 -0700

    first

--- app/src/main/java/com/example/epubreader/MainActivity.java
+++ app/src/main/java/com/example/epubreader/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.epubreader;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case  R.id.help:
+                Toast.makeText(this,"Help",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.about:
+                Toast.makeText(this,"About",Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
